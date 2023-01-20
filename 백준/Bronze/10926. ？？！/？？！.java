96901d551c91fefda9d07632f13d093bc01c14ac
Heedong <121998174+Heedong0926@users.noreply.github.com>
2023-01-20 16:10:11 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String ID = sc.next();
+		
+		System.out.println(ID + "??!");
+		sc.close();
+		 
+	}
+
+}
