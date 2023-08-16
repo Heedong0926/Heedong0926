9ae765a1d3cc958b8173beb734f02a63063c94bc
Seoyoon Kim <121998174+Heedong0926@users.noreply.github.com>
2023-08-16 11:17:37 +0900

    [unrated] Title: 코드 처리하기, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        char mode = '0';
+        
+        for(int i = 0; i < code.length(); i++){
+            if(code.charAt(i) == '1'){
+                mode = mode == '0' ? '1' : '0';
+                continue;
+            }
+            
+            if(mode == '0'){
+                if(i % 2 == 0) {
+                    answer += code.charAt(i);
+                }
+            } else {
+                if(i % 2 == 1) {
+                    answer += code.charAt(i);
+                }
+            }
+            
+        } 
+        
+        if(answer == null || answer.trim().isEmpty()){
+            answer = "EMPTY";
+        }
+        
+        return answer;
+    }
+}
